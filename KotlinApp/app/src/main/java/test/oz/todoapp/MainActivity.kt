87bb4db28f9def8a4afe1b9523ca01caa8ecb57a
oz3110 <gitozlarakot@gmail.com>
2019-03-02 17:56:00 +0900

    file upload

---
+++ KotlinApp/app/src/main/java/test/oz/todoapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package test.oz.todoapp
+
+import android.content.DialogInterface
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main
+    }
+}
