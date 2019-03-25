33b13e258922c0f2bbd06322d646ff5213fc6f61
oz3110 <gitozlarakot@gmail.com>
2019-03-25 20:01:08 +0900

    add Laravel Source Files

---
+++ Laravel/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+/*
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});*/
+
+Route::get( '/users/id/{id}', 'UserController@show');
+Route::get( '/users/email/{email}/password/{password}', 'UserController@showLogin');
+Route::post( '/users/email/{email}/name/{name}/password/{password}', 'UserController@storeUser');
+Route::post( '/users/email/{email}/name/{name}/password/{password}/{User}', 'UserController@update');
+Route::resource( '/users', 'UserController', ['except' => ['create', 'edit']]);
+Route::resource( '/items', 'ItemController', ['except' => ['create', 'edit']]);
