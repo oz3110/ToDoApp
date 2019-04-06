913ef19a2db7f60d9a2489f9b2f44babe2ef3730
oz3110 <gitozlarakot@gmail.com>
2019-04-06 12:04:02 +0900

    マイグレーションファイルとシードファイル追加

---
+++ Laravel/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+        
+        $user = new \App\User();
+        $user->name = $faker->name;
+        $user->email = $faker->unique()->safeEmail;
+        $user->password = bcrypt('password');
+        $user->remember_token = str_random(10);
+        $user->save();
+        
+        $item = new \App\Item();
+        $item->user_id = $user->id;
+        $item->content = $faker->text();
+        $item->save();
+        // $this->call(UsersTableSeeder::class);
+    }
+}
